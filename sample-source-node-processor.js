8d0b1cd611903af32ff721bec09a33c7aa52c757
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2024-04-03 20:17:23 +0300

    Kinda working POC

---
+++ sample-source-node-processor.js
@@ -0,0 +1,77 @@
+class SampleSourceNodeProcessor extends AudioWorkletProcessor {
+	constructor(options) {
+		super()
+		this.buffer_data = options.processorOptions.buffer_data;
+
+		this.start_i = currentFrame;
+		this.loopEnd_i = this.buffer_data[0].length;
+
+		/*
+		this.start_asap = false;
+		this.port.onmessage = (msg) => {
+			let t = msg.type;
+			if(t == "start_asap") {
+				this.start_asap = true;
+			}
+		};*/
+	}
+
+	static get parameterDescriptors() {
+		// TODO: Could be a-rate
+		// TODO: Using negative values for disabling these. A bit ugly
+		// but makes things simpler.
+		return [
+			{name: "startAt", defaultValue: -1.0, automationRate: 'k-rate'},
+			{name: "loopEnd", defaultValue: -1.0, automationRate: 'k-rate'},
+		]
+	}
+
+	process(inputs, outputs, parameters) {
+		//console.log("Processing", {inputs, outputs, parameters});
+		//console.log({currentFrame, currentTime, sampleRate});
+		
+
+		let output = outputs[0];
+		let startAt = parameters.startAt[0];
+		if(startAt < 0) {
+			// TODO: We skip playing rest of the buffer.
+			// TODO: We could return false here, but I don't know if this
+			// can be revived?
+			return true;
+		}
+
+		let loopEnd = parameters.loopEnd[0];
+		
+		let startAt_i = Math.round(startAt*sampleRate);
+		let loopEnd_i = Math.round(loopEnd*sampleRate);
+
+		for(let ch=0; ch < outputs.length; ++ch) {
+			let out = output[ch];
+			let buf = this.buffer_data[ch];
+			for(let i=0; i < out.length; ++i) {
+				let source_i = startAt_i + currentFrame + i;
+				// Loop back the buffer if we are looping
+				if(loopEnd_i > 0) {
+					source_i = source_i % loopEnd_i;
+				}
+
+				// Can this get missed? Is this expensive here?
+				// We can miss the first play in the loop?
+				if(source_i == 0 && ch == 0) {
+					this.port.postMessage({
+						type: "sample_start",
+						playbackTime: currentTime + sampleRate*i
+					});
+				}
+				
+				if(source_i < buf.length) {
+					out[i] += buf[source_i];
+				}
+			}
+		}
+
+		return true;
+	}
+}
+
+registerProcessor("sample-source-node-processor", SampleSourceNodeProcessor);
---
+++ samplesourcenode.js
@@ -0,0 +1,34 @@
+// TODO: Where to load this?
+
+class SampleSourceNode_ extends AudioWorkletNode {
+	constructor(context, options) {
+		// TODO: We should probably await, but no can do on a constructor of course
+		// TODO: Configure outputs based on the input sample
+		
+		let buffer = options.buffer;
+		let channels = [];
+		for(let i=0; i < buffer.numberOfChannels; ++i) {
+			channels.push(buffer.getChannelData(i));
+		}
+
+		super(context, "sample-source-node-processor", {
+			processorOptions: {buffer_data: channels},
+			outputChannelCount: [channels.length],
+		});
+
+		for(let [k, v] of this.parameters.entries()) {
+			this[k] = v
+		}
+	}
+
+	start(at) {
+		at = at ?? this.context.currentTime
+		this.startAt.value = at
+	}
+}
+
+async function SampleSourceNode(context, options) {
+	// Probably has to be done only once?
+	await context.audioWorklet.addModule('sample-source-node-processor.js')
+	return new SampleSourceNode_(context, options);
+}
